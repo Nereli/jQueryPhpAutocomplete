5b3832bb495f51889f2964ebec7851a786cdde10
Nereli <34899121+Nereli@users.noreply.github.com>
2017-12-30 14:06:56 +0200

    New

---
+++ Result.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Result</title>
+	<style type="text/css">
+		*{
+			font-family: arial;
+			font-size: 16px;
+		}
+	</style>
+</head>
+<body>
+
+	<center><h3>Searched Word: <span style="color: #ff4f4f"><?=$_POST['Word']?></span></h3></center>
+	
+</body>
+</html>
